28e4fe9c0275b7071bcdb802a452c008c73d0e72
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-25 20:16:01 +0500

    some clean 3

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ if not Path(Path.cwd() / 'model').exists():
     load_models.load_model_en_ru()
     load_models.load_model_ru_en()
 
-tokenizer_en = AutoTokenizer.from_pretrained(Path.cwd() / 'model' / 'en_ru_local')
-model_en = AutoModelForSeq2SeqLM.from_pretrained(Path.cwd() / 'model' / 'en_ru_local')
-tokenizer_ru = AutoTokenizer.from_pretrained(Path.cwd() / 'model' / 'ru_en_local')
-model_ru = AutoModelForSeq2SeqLM.from_pretrained(Path.cwd() / 'model' / 'ru_en_local')
+tokenizer_en = AutoTokenizer.from_pretrained(Path.cwd() 
+                                             / 'model' 
+                                             / 'en_ru_local')
+model_en = AutoModelForSeq2SeqLM.from_pretrained(Path.cwd() 
+                                                 / 'model' 
+                                                 / 'en_ru_local')
+tokenizer_ru = AutoTokenizer.from_pretrained(Path.cwd() 
+                                             / 'model' 
+                                             / 'ru_en_local')
+model_ru = AutoModelForSeq2SeqLM.from_pretrained(Path.cwd() 
+                                                 / 'model' 
+                                                 / 'ru_en_local')
 
 
 def translate_phrase(phrase):
@@ -33,17 +41,17 @@ def translate_phrase(phrase):
         return translate_phrase_ru(phrase)
     else:
         return translate_phrase_en(phrase)
-
+        return translate_phrase_en(phrase)
 
 def translate_phrase_ru(phrase):
     inputs = tokenizer_ru(phrase, return_tensors="pt")
     output = model_ru.generate(**inputs, max_new_tokens=100)
-    out_text = tokenizer_ru.batch_decode(output, skip_special_tokens = True)
+    out_text = tokenizer_ru.batch_decode(output, skip_special_tokens=True)
     return out_text[0]
 
 
 def translate_phrase_en(phrase):
     inputs = tokenizer_en(phrase, return_tensors="pt")
     output = model_en.generate(**inputs, max_new_tokens=100)
-    out_text = tokenizer_en.batch_decode(output, skip_special_tokens = True)
+    out_text = tokenizer_en.batch_decode(output, skip_special_tokens=True)
     return out_text[0]
--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import main 
 
 
-def test_read_main_1(): 
+def test_read_main_1():
     phrase = 'I love machine learning!'
     assert main.translate_phrase(phrase) == 'Я люблю машинное обучение!'
 
@@ -11,41 +11,41 @@ def test_read_main_2():
     assert main.translate_phrase(phrase) == 'I love machine learning!'
 
 
-def test_read_main_3(): 
+def test_read_main_3():
     phrase = None
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_4(): 
+def test_read_main_4():
     phrase = ''
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_5(): 
+def test_read_main_5():
     phrase = '2142112312423'
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_6(): 
+def test_read_main_6():
     phrase = '../,.,/.,/.,'
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_7(): 
+def test_read_main_7():
     phrase = '423434??///?...!!'
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_8(): 
+def test_read_main_8():
     phrase = '           '
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_9(): 
+def test_read_main_9():
     phrase = '!!???!??!?!'
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
 
 
-def test_read_main_10(): 
+def test_read_main_10():
     phrase = '423434      ??///?...!!'
     assert main.translate_phrase(phrase) == 'Error: Некорректная фраза для перевода'
